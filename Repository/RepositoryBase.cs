9242ad744e0c8c9f3309e0fa2d4b94e6ea40941a
Milad <miladabdi1374@gmail.com>
2022-12-08 13:22:04 +0330

    add base repository

---
+++ Repository/RepositoryBase.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq.Expressions;
+using Contracts;
+using Microsoft.EntityFrameworkCore;
+
+namespace Repository;
+
+public abstract class RepositoryBase<T> : IRepositoryBase<T> where T : class
+{
+    protected RepositoryContext context;
+
+    public RepositoryBase(RepositoryContext context)
+    {
+        this.context = context;
+    }
+
+    public IQueryable<T> FindAll(bool trackChanges) =>
+        !trackChanges
+            ? context.Set<T>().AsNoTracking()
+            : context.Set<T>();
+
+    public IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression, bool trackChanges) =>
+        !trackChanges
+            ? context.Set<T>()
+                .Where(expression)
+                .AsNoTracking()
+            : context.Set<T>()
+                .Where(expression);
+
+    public void Create(T entity) => context.Set<T>().Add(entity);
+
+    public void Update(T entity) => context.Set<T>().Update(entity);
+
+    public void Delete(T entity) => context.Set<T>().Remove(entity);
+}
