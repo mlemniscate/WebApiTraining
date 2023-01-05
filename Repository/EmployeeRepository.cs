66043cb642ffdddf98222d8441e390d0d2ca753a
Milad <miladabdi1374@gmail.com>
2023-01-05 12:37:27 +0330

    change repository paging implementation to improve performance in large amount of data in employees

--- Repository/EmployeeRepository.cs
+++ Repository/EmployeeRepository.cs
@@ -15,14 +15,15 @@ public class EmployeeRepository : RepositoryBase<Employee>, IEmployeeRepository
     public async Task<PagedList<Employee>> GetEmployeesAsync(Guid companyId,
         EmployeeParameters employeeParameters, bool trackChanges)
     {
-        var employees =
+        var employees = 
             await FindByCondition(e => e.CompanyId.Equals(companyId), trackChanges)
                 .OrderBy(e => e.Name)
-                .ToListAsync();
-        return PagedList<Employee>
-            .ToPagedList(employees, 
-                employeeParameters.PageNumber,
-                employeeParameters.PageSize);
+                .Skip((employeeParameters.PageNumber - 1) * employeeParameters.PageSize)
+                .Take(employeeParameters.PageSize).ToListAsync();
+        var count = await FindByCondition(e => 
+            e.CompanyId.Equals(companyId), trackChanges).CountAsync();
+        return new PagedList<Employee>(employees, count, 
+            employeeParameters.PageNumber, employeeParameters.PageSize);
     }
 
     public async Task<Employee> GetEmployeeAsync(Guid companyId, Guid id, bool trackChanges) =>
