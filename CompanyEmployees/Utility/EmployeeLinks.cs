392207fb82ad45c290c3348b377856e9f9b23334
Milad <miladabdi1374@gmail.com>
2023-01-07 19:51:35 +0330

    add EmployeeLinks but implement incomplete

---
+++ CompanyEmployees/Utility/EmployeeLinks.cs
@@ -0,0 +1,62 @@
+﻿using Contracts;
+using Entities.LinkModels;
+using Entities.Models;
+using Microsoft.Net.Http.Headers;
+using Shared.DataTransferObjects;
+
+namespace CompanyEmployees.Utility;
+
+public class EmployeeLinks : IEmployeeLinks
+{
+    private readonly LinkGenerator linkGenerator;
+    private readonly IDataShaper<EmployeeDto> dataShaper;
+
+    public EmployeeLinks(LinkGenerator linkGenerator, IDataShaper<EmployeeDto> dataShaper)
+    {
+        this.linkGenerator = linkGenerator;
+        this.dataShaper = dataShaper;
+    }
+
+
+    public LinkResponse TryGenerateLinks(IEnumerable<EmployeeDto> employeesDto, string fields, Guid companyId, HttpContext httpContext)
+    {
+        var shapedEmployees = ShapeData(employeesDto, fields);
+        
+        if (ShouldGenerateLinks(httpContext)) 
+            return ReturnLinkdedEmployees(employeesDto.ToList(), fields, companyId,
+                httpContext, shapedEmployees); 
+        
+        return ReturnShapedEmployees(shapedEmployees);
+    }
+
+    private List<Entity> ShapeData(IEnumerable<EmployeeDto> employeesDto, string fields) =>
+        dataShaper.ShapeData(employeesDto, fields)
+            .Select(e => e.Entity)
+            .ToList();
+
+    private bool ShouldGenerateLinks(HttpContext httpContext)
+    {
+        var mediaType = (MediaTypeHeaderValue)httpContext.Items["AcceptHeaderMediaType"];
+
+        return mediaType.SubTypeWithoutSuffix.EndsWith("hateoas",
+            StringComparison.InvariantCultureIgnoreCase);
+    }
+
+    private LinkResponse ReturnShapedEmployees(List<Entity> shapedEmployees) => new() { ShapedEntities = shapedEmployees };
+
+    private LinkResponse ReturnLinkdedEmployees(List<EmployeeDto> employeesDto, string fields,
+        Guid companyId, HttpContext httpContext, List<Entity> shapedEmployees)
+    {
+        for (int i = 0; i < employeesDto.Count; i++)
+        {
+            var employeeLink = CreateLinksForEmployee(httpContext, companyId,
+                employeesDto[i].Id, fields);
+            shapedEmployees[i].Add("Links", employeeLink);
+        }
+
+        var employeeCollection = new LinkCollectionWrapper<Entity>(shapedEmployees);
+        var linkedEmployees = CreateLinksForEmployees(httpContext, employeeCollection);
+
+        return new LinkResponse { HasLinks = true, LinkedEntities = linkedEmployees };
+    }
+}
---
+++ Contracts/IEmployeeLinks.cs
@@ -0,0 +1,10 @@
+﻿using Entities.LinkModels;
+using Shared.DataTransferObjects;
+
+namespace Contracts;
+
+public interface IEmployeeLinks
+{
+    LinkResponse TryGenerateLinks(IEnumerable<EmployeeDto> employeesDto,
+        string fields, Guid companyId, HttpContext httpContext);
+}
